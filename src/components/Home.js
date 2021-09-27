1bd76ea44748c03ae9725338cbf9c8cdda2e9a46
afsanaeva <afsanaeva05@gmail.com>
2021-09-27 15:37:56 +0600

    added other section with dynamic data

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,55 @@ import Section from './Section'
 function Home() {
     return (
         <Container>
-           <Section/>
-           <Section/>
-           <Section/>
-           <Section/>
+           <Section
+               title="Model S"
+               description="Order Online for Touchless Delivery"
+               backgroundImg="model-s.jpg"
+               LeftButtonText="Custom Order"
+               RightButtonText="Existing Inventory"
+           />
+           <Section
+               title="Model Y"
+               description="Order Online for Touchless Delivery"
+               backgroundImg="model-y.jpg"
+               LeftButtonText="Custom Order"
+               RightButtonText="Existing Inventory"
+           />
+           <Section
+               title="Model 3"
+               description="Order Online for Touchless Delivery"
+               backgroundImg="model-3.jpg"
+               LeftButtonText="Custom Order"
+               RightButtonText="Existing Inventory"
+           />
+           <Section
+               title="Model X"
+               description="Order Online for Touchless Delivery"
+               backgroundImg="model-x.jpg"
+               LeftButtonText="Custom Order"
+               RightButtonText="Existing Inventory"
+           />
+           <Section
+               title="Solar Panels"
+               description="Lowest Cost Solar Panels in America"
+               backgroundImg="solar-panel.jpg"
+               LeftButtonText="Order Now"
+               RightButtonText="Learn More"
+           />
+           <Section
+               title="Solar Roof"
+               description="Produce Clean Energy From Your Roof"
+               backgroundImg="solar-roof.jpg"
+               LeftButtonText="Order Now"
+               RightButtonText="Learn More"
+           />
+           <Section
+               title="Accessories"
+               description=""
+               backgroundImg="accessories.jpg"
+               LeftButtonText="Shop Now"
+           />
+          
         </Container>
     )
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,32 +1,35 @@
 import React from 'react'
 import styled from "styled-components"
 
-function Section() {
+function Section({ title, description, LeftButtonText, RightButtonText, backgroundImg }) {
     return (
-     <Wrap>
-         <ItemText>
-             <h1>Model S</h1>
-             <p>Order Online for Touches Delivery</p>
-         </ItemText>
-         <Buttons>
-         <ButtonGroup>
-         <LeftButton>
-            Custom Order
-         </LeftButton>
-         <RightButton>
-            Existing Inventory
-         </RightButton>
+        <Wrap bgImage={backgroundImg}>
+            <ItemText>
+                <h1>{title}</h1>
+                <p>{description}</p>
+            </ItemText>
+            <Buttons>
+                <ButtonGroup>
+                    <LeftButton>
+                        {LeftButtonText}
+                    </LeftButton>
+                    {RightButtonText && 
+                        <RightButton>
+                            {RightButtonText}
+                        </RightButton>
+                    }
 
-         </ButtonGroup>
-         <DownArrow src="./images/down-arrow.svg"></DownArrow>
-         </Buttons>
-     </Wrap>
+
+                </ButtonGroup>
+                <DownArrow src="./images/down-arrow.svg"></DownArrow>
+            </Buttons>
+        </Wrap>
     )
 }
 
 export default Section
 const Wrap = styled.div
-`
+    `
 width:100vw;
 height:100vh;
 background-size:cover;
@@ -37,20 +40,23 @@ display:flex;
 flex-direction:column;
 justify-content:space-between;//for vertical
 align-items:center;//for horizontal
-
+background-image: ${props => `url("/images/${props.bgImage}")`}
 `
 const ItemText = styled.div
-`
+    `
 padding-top:15vh;
 text-align:center;
 `
 
 const ButtonGroup = styled.div
-`display: flex;
-margin-bottom:30px;`
+    `display: flex;
+margin-bottom:30px;
+@media (max-width: 768px) { 
+   flex-direction:column;
+  }`
 
-const LeftButton= styled.div
-`background-color:rgba(23,26,32,0.8);
+const LeftButton = styled.div
+    `background-color:rgba(23,26,32,0.8);
 height:40px;
 width:256px;
 color:white;
@@ -64,16 +70,16 @@ font-size:12px;
 cursor:pointer;
 margin:8px
 `
-const RightButton= styled(LeftButton)`
+const RightButton = styled(LeftButton)`
 background:white;
 opacity:0.65;
 color:black;
 `
-const DownArrow=styled.img
-`margin-top:20px;
+const DownArrow = styled.img
+    `margin-top:20px;
 height:40px;
 overflow-x:hidden;
 animation: animationDown infinite 1.5s`
 
-const Buttons =styled.div
-`;`
+const Buttons = styled.div
+    `;`
